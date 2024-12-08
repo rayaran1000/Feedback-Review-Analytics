2ed97c8bc05cad647aa58b56144c256ec0cee0b2
rayaran1000 <aranya.ray1998@gmail.com>
2024-12-08 16:06:37 +0530

    UI Changes

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -8,3 +8,24 @@ export enum UserRole {
   USER = "user",
   ADMIN = "admin"
 }
+
+export interface LoginResponse {
+  access_token: string;
+  token_type: string;
+}
+
+export interface UserData {
+  username: string;
+  role: UserRole;
+}
+
+export interface FeedbackItem {
+  feedback: string;
+  timestamp: string;
+  username: string;
+}
+
+export interface FeedbackResponse {
+  current: FeedbackItem[];
+  historical: FeedbackItem[];
+}
